65d6ce5f05e59314c34f08368932edb122c0f094
Brenna O'Brien <info@brennaobrien.com>
2013-03-10 21:54:54 -0400

    cleanup system files

--- _site/assets/js/main-2d0e45e058c24f91486decfa9866b07d.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	
-	// outside links open in new tab	
-	$(document.links).filter(function() {
-    	return this.hostname != window.location.hostname;
-	}).attr('target', '_blank');
-
-});
