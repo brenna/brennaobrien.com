361afc6f36185150a108cb1af299cad5b26ef001
brenna <info@brennaobrien.com>
2013-04-26 15:36:34 -0400

    smooth scroll and text bounce for contact link/footer

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,11 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	
-	// outside links open in new tab	
+	// outside links open in new tab
 	$(document.links).filter(function() {
-    	return this.hostname != window.location.hostname;
-	}).attr('target', '_blank');
+		return this.hostname != window.location.hostname;
+    }).attr('target', '_blank');
 
 	//activate fitvids.js for blog posts
 	$('.blog-content').fitVids();
+
+
+	//smooth scroll to contact and activate CSS bouncy text
+	$('#contact-link').click(function() {
+		$('html,body').animate({
+			scrollTop: $('#contact').offset().top
+		}, 500, function() {
+			//trigger CSS animation
+			$("#contact h3").addClass('animate').bind('oanimationend animationend webkitAnimationEnd', function() {
+				//reset the animation once complete
+				$(this).removeClass('animate');
+			});
+		});
+		return false;
+	});
 
 });
