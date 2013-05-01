7dba96d422ef4ef6e56936e2562535b1344d0817
brenna <info@brennaobrien.com>
2013-04-30 23:59:36 -0400

    add Google analytics UA id

--- _site/assets/main.js
+++ _site/assets/main.js
@@ -11,7 +11,6 @@ $(document).ready(function() {
 
 	//smooth scroll to contact and activate CSS bouncy text
 	$('#contact-link').click(function() {
-
 		$('html,body').animate({
 			scrollTop: $('#contact').offset().top
 		}, 500, function() {
