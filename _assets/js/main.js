015f1889b5582d0b37e1bef56e7c8cb5daf9ab1c
brenna <info@brennaobrien.com>
2013-04-30 23:42:55 -0400

    possible fix for mobile anchor links getting stuck in focussed state

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
 
 	//smooth scroll to contact and activate CSS bouncy text
 	$('#contact-link').click(function() {
+		$(this).blur(); //mobile browsers retain focus state, kill it
 		$('html,body').animate({
 			scrollTop: $('#contact').offset().top
 		}, 500, function() {
